777d37f0685aad389b1470df2972a1ea67b5a444
Ahad2906 <s175128@student.dtu.dk>
2019-05-06 14:27:28 +0200

    spawn startet

---
+++ TopDownShooter_Test1/Assets/Scripts/Spawner.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner: MonoBehaviour {
+
+    public Wave[] waves;
+    public EnemyController enemy;
+
+    Wave currentWave;
+    int currentWaveNumber;
+
+    int enemieRemainingToSpawn;
+    float nextSpawnTime;
+
+    private void Start() {
+        nextWave();
+    }
+
+    void Update() {
+        if (enemieRemainingToSpawn > 0 && Time.time > nextSpawnTime) {
+            enemieRemainingToSpawn--;
+            nextSpawnTime = Time.time + currentWave.spawnInterval;
+
+            EnemyController spawnedEnemy = Instantiate(enemy, Vector3.zero, Quaternion.identity) as EnemyController;
+        }
+    }
+
+    void nextWave() {
+        currentWaveNumber++;
+        currentWave = waves[currentWaveNumber - 1];
+
+        enemieRemainingToSpawn = currentWave.enemyCount;
+    }
+
+
+    [System.Serializable]
+    public class Wave {
+
+        public int enemyCount;
+        public float spawnInterval;
+
+    }
+
+}
