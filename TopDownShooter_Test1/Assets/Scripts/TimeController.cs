91013b42a09470b563512a2ea19cd61e5c0b5fea
Ahad2906 <s175128@student.dtu.dk>
2019-05-14 00:31:02 +0200

    (Esc || joystick.Options) ==> pause game

---
+++ TopDownShooter_Test1/Assets/Scripts/TimeController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeController : MonoBehaviour
+{
+
+    private bool paused = false;
+
+    void Update()
+    {
+        
+        if(Input.GetKeyDown(KeyCode.Escape) || (Input.GetKeyDown(KeyCode.JoystickButton9)))
+        {
+            if (paused)
+                Time.timeScale = 1;
+            else
+                Time.timeScale = 0;
+
+            paused = !paused;
+
+        }
+
+    }
+}
